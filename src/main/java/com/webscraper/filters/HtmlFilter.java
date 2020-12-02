de28c721a33b0916c290eff38d69878c16285773
joshgvines <joshgvines@gmail.com>
2020-12-01 21:13:58 -0600

    Adding image regex to filter enum

--- src/main/java/com/webscraper/filters/HtmlFilter.java
+++ src/main/java/com/webscraper/filters/HtmlFilter.java
@@ -3,10 +3,8 @@ package com.webscraper.filters;
 public enum HtmlFilter {
 
     FIND_PARAGRAPH("<p(.*?)p>"),
-    END_PARAGRAPH("\\b</p>|/p>"),
 
-    FIND_IMAGE("\\b<img|<img>"),
-    END_IMAGE("\\b</img>|/>"),
+    FIND_IMAGE("<img(.*?)>"),
 
     FIND_ANY_HTTP_URL("\\(?\\b(http://|www[.])[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]"),
     FIND_ANY_HTTPS_URL("\\(?\\b(https://|www[.])[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]");
--- src/main/java/com/webscraper/requests/ImageRequest.java
+++ src/main/java/com/webscraper/requests/ImageRequest.java
@@ -1,16 +1,49 @@
 package com.webscraper.requests;
 
+import com.webscraper.filters.HtmlFilter;
+import com.webscraper.managers.ClientRequestManager;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ImageRequest implements Request {
+
+    private static ArrayList<String> images;
 
     @Override
     /**
      *
      */
-    public void execute(String pageString) {
-
+    public void execute(String givenUrl) {
+        System.out.println("Image Request");
+        String page = ClientRequestManager.attemptClientRequest(givenUrl);
+        //String[] lines = pageString.split(System.getProperty("line.separator"));
+        if (findImages(page)) {
+            outputLinks();
+        }
     }
 
     public boolean isValid() {
         return true;
+    }
+
+    private static boolean findImages(String page) {
+        images = new ArrayList();
+        Pattern p = Pattern.compile(HtmlFilter.FIND_IMAGE.getFilter());
+        Matcher m = p.matcher(page);
+        while (m.find()) {
+            String link = m.group();
+            if (!images.contains(link)) {
+                images.add(m.group());
+            }
+        }
+        return !( images.isEmpty() );
+    }
+
+    private static void outputLinks() {
+        for (String link : images) {
+            System.out.println(link);
+        }
     }
 }
